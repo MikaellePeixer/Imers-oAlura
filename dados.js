a0484725736a3009739a3f4dd9b984cb5761e80c
Mikaelle da Silva Peixer <157852660+MikaellePeixer@users.noreply.github.com>
2024-09-05 23:31:33 -0300

    Add files via upload
    git commit -m "Primeira commit do projeto"

---
+++ dados.js
@@ -0,0 +1,17 @@
+let dados = [
+    {
+        titulo: "One Piece",
+        descricao: "One Piece é uma jornada épica que acompanha Monkey D. Luffy, um jovem pirata com o sonho de encontrar o One Piece, o maior tesouro do mundo e se tornar o Rei dos Piratas. Junto de sua excêntrica tripulação, o Chapéu de Palha, Luffy navega pelos Grand Line, enfrentando piratas poderosos, desvendando mistérios e fazendo amigos inesquecíveis. A série é conhecida por sua história extensa e cheia de aventuras, humor, ação e momentos emocionantes.",
+        link: "https://www.netflix.com/watch/80107103?source=35"
+    },
+    {
+        titulo: "Dr. Stone",
+        descricao: "Em Dr. Stone, a humanidade é petrificada por um misterioso flash de luz. Milhares de anos depois, o gênio científico Senku Ishigami desperta e decide reconstruir a civilização usando apenas a ciência. Com a ajuda de seu amigo Taiju Oki, Senku inicia uma jornada para reunir pessoas e recursos, revivendo a humanidade um por um.",
+        link: "https://www.netflix.com/watch/81046193?source=35"
+    },
+    {
+        titulo: "Bleach",
+        descricao: "Bleach acompanha Ichigo Kurosaki, um adolescente que se torna um Shinigami substituto, um protetor espiritual que luta contra hollows, espíritos malignos que devoram almas humanas. Junto de seus amigos, Ichigo se envolve em uma guerra entre diferentes mundos e descobre segredos sobre sua própria origem.",
+        link: "https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwimh5eF3aqIAxWPrZUCHXiPNbYQFnoECDcQAQ&url=https%3A%2F%2Fwww.crunchyroll.com%2Fpt-br%2Fseries%2FG63VGG2NY%2Fbleach%3Fsrsltid%3DAfmBOoo-1LkBoFzWNfTxBfqvTKJF36ztVZLVc6lKAf8Bj2RqB7nX1CWe&usg=AOvVaw1DRENoNP5gS-6hFbecm8so&opi=89978449"
+    }
+];
