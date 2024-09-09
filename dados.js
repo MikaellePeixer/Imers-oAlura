26325cf1900fe3a3dbc1ccc6863c43f61c4d5368
Mikaelle da Silva Peixer <157852660+MikaellePeixer@users.noreply.github.com>
2024-09-09 14:06:01 -0300

    Add files via upload

--- dados.js
+++ dados.js
@@ -13,5 +13,137 @@ let dados = [
         titulo: "Bleach",
         descricao: "Bleach acompanha Ichigo Kurosaki, um adolescente que se torna um Shinigami substituto, um protetor espiritual que luta contra hollows, espíritos malignos que devoram almas humanas. Junto de seus amigos, Ichigo se envolve em uma guerra entre diferentes mundos e descobre segredos sobre sua própria origem.",
         link: "https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwimh5eF3aqIAxWPrZUCHXiPNbYQFnoECDcQAQ&url=https%3A%2F%2Fwww.crunchyroll.com%2Fpt-br%2Fseries%2FG63VGG2NY%2Fbleach%3Fsrsltid%3DAfmBOoo-1LkBoFzWNfTxBfqvTKJF36ztVZLVc6lKAf8Bj2RqB7nX1CWe&usg=AOvVaw1DRENoNP5gS-6hFbecm8so&opi=89978449"
+    },
+    {
+        titulo: "Naruto",
+        descricao: "Naruto Uzumaki é um jovem ninja que busca reconhecimento e sonha em se tornar o Hokage, o líder da vila da Folha. Acompanhe sua jornada repleta de batalhas, amizades e descobertas sobre si mesmo.",
+        link: "https://www.crunchyroll.com/pt-br/series/G63VGG2N4/naruto"
+    },
+    {
+        titulo: "My Hero Academia",
+        descricao: "Em um mundo onde a maioria das pessoas possui superpoderes, Izuku Midoriya sonha em se tornar um herói, mesmo sem ter nenhuma habilidade especial. Sua vida muda quando ele conhece o maior herói de todos, All Might, e recebe um poder incrível.",
+        link: "https://www.crunchyroll.com/pt-br/series/G63VGG2N5/my-hero-academia"
+    },
+    {
+        titulo: "Attack on Titan",
+        descricao: "Em um mundo onde a humanidade vive dentro de muralhas para se proteger de titãs gigantes, Eren Yeager jura exterminar todos os titãs após presenciar a morte de sua mãe. Junte-se a Eren e seus amigos em uma luta pela sobrevivência.",
+        link: "https://www.crunchyroll.com/pt-br/series/G63VGG2N6/attack-on-titan"
+    },
+    {
+        titulo: "Demon Slayer",
+        descricao: "Tanjiro Kamado, um jovem carvoeiro, vê sua família massacrada por demônios e sua irmã mais nova transformada em um deles. Ele se junta aos Caçadores de Demônios para encontrar uma cura para sua irmã e vingar sua família.",
+        link: "https://www.crunchyroll.com/pt-br/series/G63VGG2N7/demon-slayer-kimetsu-no-yaiba"
+    },
+    {
+        titulo: "Fullmetal Alchemist: Brotherhood",
+        descricao: "Os irmãos Edward e Alphonse Elric tentam trazer sua mãe de volta à vida através da alquimia, mas pagam um preço alto por sua imprudência. Acompanhe sua jornada para recuperar seus corpos perdidos e desvendar os segredos da alquimia.",
+        link: "https://www.crunchyroll.com/pt-br/series/G63VGG2N8/fullmetal-alchemist-brotherhood"
+    },
+    {
+        titulo: "Cowboy Bebop",
+        descricao: "Em um futuro distante, Spike Spiegel, um caçador de recompensas, vaga pela galáxia em sua nave espacial junto com sua equipe excêntrica. Acompanhe as aventuras desse grupo de anti-heróis enquanto eles caçam criminosos e lidam com seus próprios problemas.",
+        link: "https://www.netflix.com/watch/80113630?source=35"
+    },
+    {
+        titulo: "Death Note",
+        descricao: "Light Yagami, um estudante do ensino médio, encontra um caderno misterioso com o poder de matar qualquer pessoa cujo nome seja escrito nele. Obcecado com a ideia de criar um mundo sem crimes, Light inicia uma guerra contra o criminoso conhecido como Kira.",
+        link: "https://www.netflix.com/watch/80111128?source=35"
+    },
+    {
+        titulo: "Hunter x Hunter",
+        descricao: "Gon Freecss, um garoto sonhador, decide se tornar um Hunter, um caçador profissional que busca tesouros, criaturas lendárias e a verdade sobre o mundo. Junte-se a Gon em sua jornada épica enquanto ele faz amigos, enfrenta desafios e descobre os segredos de sua família.",
+        link: "https://www.crunchyroll.com/pt-br/series/G63VGG2NA/hunter-x-hunter"
+    },
+    {
+        titulo: "Yu Yu Hakusho",
+        descricao: "Yusuke Urameshi, um adolescente delinquente, morre ao salvar uma criança de um acidente. Diante de sua morte inesperada, ele recebe uma segunda chance de viver como um detetive espiritual. Acompanhe Yusuke em suas aventuras enquanto ele luta contra demônios e espíritos malignos para proteger o mundo humano.",
+        link: "https://www.crunchyroll.com/pt-br/series/G63VGG2N9/yu-yu-hakusho"
+    },
+    {
+        titulo: "Mob Psycho 100",
+        descricao: "Shigeo Kageyama, conhecido como Mob, é um jovem com habilidades psíquicas extraordinárias. Acompanhe sua jornada enquanto ele lida com seus poderes, enfrenta desafios e busca seu próprio caminho.",
+        link: "https://www.crunchyroll.com/pt-br/series/G63VGG2NB/mob-psycho-100"
+    },
+    {
+        titulo: "JoJo's Bizarre Adventure",
+        descricao: "Uma saga épica que acompanha várias gerações da família Joestar em suas aventuras repletas de ação, mistério e batalhas épicas utilizando Stands, manifestações físicas da força de vontade.",
+        link: "https://www.crunchyroll.com/pt-br/series/G63VGG2NC/jojos-bizarre-adventure"
+    },
+    {
+        titulo: "Vinland Saga",
+        descricao: "Em um mundo medieval violento, Thorfinn, um jovem viking, busca vingança pela morte de seu pai. Acompanhe sua jornada em busca de paz e redenção em um mundo cruel e implacável.",
+        link: "https://www.amazon.com.br/gp/video/detail/B07Q6X49M5/ref=dvm_ov_tv"
+    },
+    {
+        titulo: "Code Geass: Lelouch of the Rebellion",
+        descricao: "Lelouch vi Britannia, um príncipe exilado, recebe um poder misterioso que lhe permite controlar as ações das pessoas. Ele inicia uma revolução para destruir o Sagrado Império de Britannia e criar um mundo melhor.",
+        link: "https://www.netflix.com/watch/80112164?source=35"
+    },
+    {
+        titulo: "Fullmetal Alchemist",
+        descricao: "Versão original de Fullmetal Alchemist, com uma história alternativa e um ritmo mais acelerado. Os irmãos Elric embarcam em uma jornada para restaurar seus corpos e encontrar a Pedra Filosofal.",
+        link: "https://www.crunchyroll.com/pt-br/series/G63VGG2ND/fullmetal-alchemist"
+    },
+    {
+        titulo: "Trigun",
+        descricao: "Vash the Stampede, um homem com uma recompensa de 60 bilhões de double dollars, viaja pelo planeta em busca de redenção. Acompanhe suas aventuras enquanto ele enfrenta caçadores de recompensas e desvenda o mistério de seu passado.",
+        link: "https://www.crunchyroll.com/pt-br/series/G63VGG2NE/trigun"
+    },
+    {
+        titulo: "Samurai Champloo",
+        descricao: "Em um Japão feudal alternativo, um samurai, um ronin e uma jovem viajam juntos em busca de um samurai que cheira a girassóis. Acompanhe suas aventuras enquanto eles enfrentam samurais, ninjas e outros adversários.",
+        link: "https://www.crunchyroll.com/pt-br/series/G63VGG2NF/samurai-champloo"
+    },
+    {
+        titulo: "Ghost in the Shell: Stand Alone Complex",
+        descricao: "Em um futuro próximo, a linha entre humanos e ciborgues se torna cada vez mais tênue. A Major Motoko Kusanagi e sua equipe da Seção 9 investigam crimes cibernéticos e conspirações em um mundo dominado pela tecnologia.",
+        link: "https://www.netflix.com/watch/81046203?source=35"
+    },
+    {
+        titulo: "Neon Genesis Evangelion",
+        descricao: "Em um futuro pós-apocalíptico, adolescentes pilotos controlam gigantescas mechas chamadas Evangelions para lutar contra seres misteriosos conhecidos como Anjos. Acompanhe a jornada psicológica dos pilotos enquanto eles enfrentam seus traumas e o destino da humanidade.",
+        link: "https://www.netflix.com/watch/80114873?source=35"
+   
+    },
+    {
+        titulo: "Spy x Family",
+        descricao: "Um espião, uma assassina e uma telepata fingindo ser uma família feliz em uma missão secreta. Uma comédia espionagem cheia de humor e ação.",
+        link: "https://www.crunchyroll.com/pt-br/series/G63VGG2NG/spy-x-family"
+    },
+    {
+        titulo: "Jujutsu Kaisen",
+        descricao: "Um jovem com uma maldição poderosa se junta a uma escola de feitiçaria para aprender a controlar seus poderes e lutar contra maldições.",
+        link: "https://www.crunchyroll.com/pt-br/series/G63VGG2NH/jujutsu-kaisen"
+    },
+    {
+        titulo: "Violet Evergarden",
+        descricao: "Em um mundo pós-guerra, Violet Evergarden, uma ex-soldado, busca um novo propósito na vida como uma Auto Memories Doll, escrevendo cartas que transmitem emoções humanas.",
+        link: "https://www.netflix.com/watch/80116116?source=35"
+    },
+    {
+        titulo: "Haikyuu!!",
+        descricao: "Um garoto baixo com um grande sonho de se tornar um jogador de vôlei profissional inspira seus companheiros de equipe e leva sua escola a novas alturas.",
+        link: "https://www.crunchyroll.com/pt-br/series/G63VGG2NI/haikyuu"
+    },
+    {
+        titulo: "Your Lie in April",
+        descricao: "Um prodígio do piano que perdeu a capacidade de ouvir retorna ao mundo da música com a ajuda de uma violinista brilhante.",
+        link: "https://www.netflix.com/watch/80114872?source=35"
+    },
+    {
+        titulo: "Mob Psycho 100 II",
+        descricao: "A continuação das aventuras de Shigeo Kageyama, explorando temas mais profundos sobre identidade, amizade e a natureza dos poderes psíquicos.",
+        link: "https://www.crunchyroll.com/pt-br/series/G63VGG2NJ/mob-psycho-100-ii"
+    },
+    {
+        titulo: "Parasyte - The Maxim",
+        descricao: "Um parasita alienígena invade o corpo de Shinichi Izumi e os dois aprendem a coexistir enquanto enfrentam outros parasitas que buscam dominar a humanidade.",
+        link: "https://www.crunchyroll.com/pt-br/series/G63VGG2NK/parasyte-the-maxim"
+    },
+    {
+        titulo: "Made in Abyss",
+        descricao: "Uma jovem exploradora desce para as profundezas de um abismo misterioso, enfrentando perigos e criaturas fantásticas em busca de sua mãe.",
+        link: "https://www.crunchyroll.com/pt-br/series/G63VGG2NL/made-in-abyss"
     }
-];
+    
+  ];
